835ca567d1fe32f7e18e006983dafe4391d66044
Vialli Gonsalves <gonsalvesvialli06@gmail.com>
2023-01-04 11:04:42 -0800

    indentation

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var express = require("express")
 var bodyParser = require("body-parser")
 var mongoose = require("mongoose")
 const { Schema } = mongoose;
-var path=require('path');
-const projectSchema = new Schema({ idea: String,name: String, market:String, continent:String, country:String }, { timestamps: true });
+var path = require('path');
+const projectSchema = new Schema({ idea: String, name: String, market: String, continent: String, country: String }, { timestamps: true });
 const Project = mongoose.model('Project', projectSchema);
 
 const app = express()
@@ -21,35 +21,35 @@ const app = express()
 //     console.log("Connected succesfully to mongodb");
 // })
 
-mongoose.connect('mongodb://127.0.0.1:27017/mydb',{
+mongoose.connect('mongodb://127.0.0.1:27017/mydb', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
 
 app.set('view engine', 'ejs')
-app.set('views',path.join(__dirname,'views'));
+app.set('views', path.join(__dirname, 'views'));
 
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
-    extended:true
+    extended: true
 }))
 
-app.get('/', (req,res) =>{
-   Project.find({}, (err, projects) => {
-     console.log(projects);
-     res.render('main', {'projects': projects})
-   })
-})
-
-app.get('/projects', (req,res) =>{
+app.get('/', (req, res) => {
     Project.find({}, (err, projects) => {
         console.log(projects);
-       res.render('projects', {'projects': projects});
+        res.render('main', { 'projects': projects })
+    })
+})
+
+app.get('/projects', (req, res) => {
+    Project.find({}, (err, projects) => {
+        console.log(projects);
+        res.render('projects', { 'projects': projects });
     })
 });
 
-app.get('/projects/:id', (req,res) =>{
+app.get('/projects/:id', (req, res) => {
     console.log(req.params);
     var id = req.params.id;
     console.log(id);
@@ -57,38 +57,38 @@ app.get('/projects/:id', (req,res) =>{
         console.log("*******");
         // console.log(project.id);
         console.log(project);
-        res.render('view_project',{'project': project });
+        res.render('view_project', { 'project': project });
     })
- });
+});
 
-app.post('/projects/:id', (req,res) =>{
+app.post('/projects/:id', (req, res) => {
     var id = req.params.id;
     var body = req.body;
 
     var data = {
         idea: body.idea,
-        name : body.name,
+        name: body.name,
         market: body.market,
-        continent : body.continent,
-        country : body.country
+        continent: body.continent,
+        country: body.country
     }
 
-    Project.updateOne({ _id: id }, data,(err,project)=>{
-        if(err){
+    Project.updateOne({ _id: id }, data, (err, project) => {
+        if (err) {
             throw err;
         }
         console.log("Record Updated Successfully");
     });
 
     res.redirect('/projects');
- });
+});
 
 
-app.get('/project/new', (req,res) =>{
+app.get('/project/new', (req, res) => {
     res.render('new_project');
 });
 
-app.post("/projects",(req,res)=>{
+app.post("/projects", (req, res) => {
     console.log("heer********************** ");
     // console.log(req);
     console.log(req.body);
@@ -101,14 +101,14 @@ app.post("/projects",(req,res)=>{
 
     var data = {
         idea: idea,
-        name : name,
+        name: name,
         market: market,
-        continent : continent,
-        country : country
+        continent: continent,
+        country: country
     }
 
-    Project.create(data,(err,project)=>{
-        if(err){
+    Project.create(data, (err, project) => {
+        if (err) {
             throw err;
         }
         console.log("Record Inserted Successfully");
