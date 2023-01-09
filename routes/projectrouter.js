b25248effe820f583281b2790b3b29e49e5230f5
Vialli Gonsalves <gonsalvesvialli06@gmail.com>
2023-01-09 09:31:24 -0800

    Changes to the formatting and indentation and JS inclusion
    -proper formatting
    
    -proper indentation
    
    -addition of js file(external)
    
    -common display of view and new file

--- routes/projectrouter.js
+++ routes/projectrouter.js
@@ -3,9 +3,9 @@ const app = Router();
 
 const ProjectController = require('../controller/projectcontroller')
 
-app.get('/projects', ProjectController.find_all );
+app.get('/projects', ProjectController.find_all);
 
-app.get('/projects/:id', ProjectController.find_one );
+app.get('/projects/:id', ProjectController.find_one);
 
 app.post('/projects/:id', ProjectController.update_one);
 
